f1b25c8ed98856404e8394e1a5ea146d51dbdd22
AllisonB <arnaudallisonb@gmail.com>
2022-12-12 19:28:13 +1100

    updated readme, created cat sorting function to rank cat breeds

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cat Api project
+
+This project uses the Cat api found at this link: https://thecatapi.com/
+
+The goal is to display the top 5 cats that rank highest in the following criteria, combined:
+1. Dog friendly
+2. Child friendly
+3. Stranger friendly
+
+The technologies used were:
+- javascript
+- expressJS
+- Axios
+- ejs
+- dotenv
+
+Note: In order to test this project, you will need your own developer api key from the cat api website, and create a .env file to hold this key.
+
+## Getting started
+
+In order to start this project, run "npm i", which will install all npm packages required. Afterwards, run "npm run devStart".
+
+
--- server.js
+++ server.js
@@ -1,22 +1,53 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.set('view engine', 'ejs')
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = process.env.API_KEY;
+
+const getCatBreeds = () => {
+  app.get('/', (req, res) => {
+    axios.get('/breeds')
+      .then(response => res.render('index', {text: response.data[0].name, image: response.data[0].image.url}))
+      .catch(error => console.log(error));
+  })
+}
+
+// sort the breeds by ranking, add the 3 values (child friendly, stranger friendly, dog friendly) and rank by highest score (top 5)
+
+const sortCatBreedsByScore = () => {
+  var catScores = [];
+  app.get('/', (req, res) => {
+    axios.get('/breeds')
+      .then(response => {
+        response.data.forEach((cat) => {
+          if(cat.image != undefined) {
+            var catDetails = [cat.name, (parseInt(cat.child_friendly)+parseInt(cat.stranger_friendly)+parseInt(cat.dog_friendly)), cat.image.url];
+            catScores.push(catDetails);
+          }
+        })
+        // sorting the cat score list in descending order
+        catScores.sort(function(a, b) {
+          return b[1] - a[1];
+        })
+        // Slicing the top 5 cats 
+        var topFiveCats = catScores.slice(0,5);
+        console.log(topFiveCats);
+        res.render('index', {catList: topFiveCats})
+      })
+      .catch(error => console.log(error));
+  })
+}
+
+// getCatBreeds()
+sortCatBreedsByScore()
 
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
 })
 
-const axios = require('axios');
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  .then(response => {
-    console.log(response.data.url);
-    console.log(response.data.explanation);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  
